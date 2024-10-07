09a0de4860318286eb30b237082e6b65571a533a
Weichen Chen <matthew.chen.office@gmail.com>
2024-10-07 15:18:40 +0800

    Add styles.py

---
+++ Reflex_Dashboard_Hanger1A/styles.py
@@ -0,0 +1,16 @@
+
+
+style: dict[str, dict[str, str]] = {
+    ".cell-border":{
+        "border":"solid",
+    },
+    ".cell-bgc-1":{
+        "background_color":"#E8FFC4"
+    },
+    ".cell-bgc-2":{
+        "background_color":"#97CBFF"
+    },
+    ".cell-bgc-3":{
+        "background_color":"#FFDAC8"
+    },
+}
