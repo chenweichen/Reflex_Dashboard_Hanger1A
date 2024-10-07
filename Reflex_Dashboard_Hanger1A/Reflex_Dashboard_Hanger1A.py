d835eff3722a9e588eeca3e951cdb91e650c29c8
Weichen Chen <matthew.chen.office@gmail.com>
2024-10-07 16:19:22 +0800

    Add data_eidt and data_presentation_tab

--- Reflex_Dashboard_Hanger1A/Reflex_Dashboard_Hanger1A.py
+++ Reflex_Dashboard_Hanger1A/Reflex_Dashboard_Hanger1A.py
@@ -1,39 +1,86 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
+#import reflex as rx
+#from rxconfig import config
+#class State(rx.State):
+#    """The app state."""
+#
+#    ...
+#def index() -> rx.Component:
+#    # Welcome Page (Index)
+#    return rx.container(
+#        rx.color_mode.button(position="top-right"),
+#        rx.vstack(
+#            rx.heading("Welcome to Reflex!", size="9"),
+#            rx.text(
+#                "Get started by editing ",
+#                rx.code(f"{config.app_name}/{config.app_name}.py"),
+#                size="5",
+#            ),
+#            rx.link(
+#                rx.button("Check out our docs!"),
+#                href="https://reflex.dev/docs/getting-started/introduction/",
+#                is_external=True,
+#            ),
+#            spacing="5",
+#            justify="center",
+#            min_height="85vh",
+#        ),
+#        rx.logo(),
+#    )
+#app = rx.App()
+#app.add_page(index)
+
+
 import reflex as rx
+import reflex_chakra as rc
 
 from rxconfig import config
 
+from .components import gen_area_1_block_cells, gen_area_2_block_cells
+from .states import MachinesState
 
-class State(rx.State):
-    """The app state."""
-
-    ...
+#from .styles import style
 
 
-def index() -> rx.Component:
-    # Welcome Page (Index)
+def data_edit_tab() -> rx.Component:
     return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
+        rx.heading(f'Data Edit Tab'),
+        rx.text(f'{MachinesState.clicked_data}'),
+        rx.text(f'{MachinesState.edited_data}'),
+        rx.data_editor(
+            columns=MachinesState.cols,
+            data=MachinesState.machine_data,
+            on_cell_clicked=MachinesState.click_cell,
+            on_cell_edited=MachinesState.handle_cell_edit,
+        )
     )
 
+def data_presentation_tab() -> rx.Component:
+    return rc.grid(
+        *gen_area_1_block_cells(),
+        *gen_area_2_block_cells(),
+        template_rows="repeat(10, 1fr)",
+        template_columns="repeat(4, 1fr)",
+        height="10em",
+        width="100%",
+        gap=2,
+    )
 
-app = rx.App()
-app.add_page(index)
+def index() -> rx.Component:
+    return rx.tabs.root(
+        rx.tabs.list(
+            rx.tabs.trigger("Data edit tab", value="tab1"),
+            rx.tabs.trigger("Presentation tab", value="tab2"),
+        ), # rx.tabs.list
+        rx.tabs.content(
+            data_edit_tab(),
+            value="tab1",
+        ),
+        rx.tabs.content(
+            data_presentation_tab(),
+            value="tab2",
+        ),
+        defalut_value="tab1",
+        orientation="horizontal"
+    ) # rx.tab.root
